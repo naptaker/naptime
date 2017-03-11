284eb4748852a276ec79c41353c05d53a799afa1
Eric Bailey <eric@ericb.me>
2017-03-11 10:29:55 -0600

    Rework Nix flow

--- default.nix
+++ default.nix
@@ -9,76 +9,97 @@ with pkgs.lib;
 
 let
   version = "0.0.1";
-  src = ./.;
 
   lilypond-with-improviso = lilypond-with-fonts.override {
     fonts = with openlilylib-fonts; [ improviso ];
   };
 
-  deletePdfs = "find songs -name '*.pdf' -delete";
+  openlilylib = fetchFromGitHub {
+    owner = "yurrriq";
+    repo = "snippets";
+    rev = "0180270";
+    sha256 = "0mahv9r12gscsykaf9r1lqpb4z0v987b1d3nyv7mznpy384bl5s5";
+  };
 
   mkSong = a@{ songName, pdfName ? songName, ... }:
     stdenv.mkDerivation (a // rec {
       name = "naptime-${songName}-${version}";
-      inherit version;
-      src = ./.;
-      patchPhase = deletePdfs;
-      buildInputs = [ lilypond-with-improviso ];
-      makeFlags = songName + optionalString debug " DEBUG=1";
-      installPhase = ''
+      inherit pdfName songName version;
+      src = ./. + "/songs/${songName}";
+
+      nativeBuildInputs = [ lilypond-with-improviso openlilylib ];
+
+      buildPhase = ''
         install -m755 -d $out
-        install -m644 songs/${songName}/main.pdf $out/${pdfName}.pdf
       '';
+
+      preInstall = ''
+        installFlagsArray=(
+          -B
+          openlilylib=${openlilylib}
+          lilypond=${lilypond-with-improviso}/bin/lilypond
+          pdfName=${pdfName}
+          prefix=$out/
+          ${optionalString debug "DEBUG=1"}
+        );
+      '';
+
       # TODO: meta
     });
 
   songs = rec {
+    cover_yr_eyes = mkSong {
+      songName = "cover_yr_eyes";
+    };
     funeral_town = mkSong {
       songName = "funeral_town";
     };
-    mountain = mkSong {
-      songName = "mountain";
-    };
+    # FIXME:
+    # mountain = mkSong {
+    #   songName = "mountain";
+    # };
     all = [
-     funeral_town
-     mountain
+      cover_yr_eyes
+      funeral_town
+      # FIXME: mountain
     ];
   };
 
   env = stdenv.mkDerivation rec {
     name = "naptime-${version}-env";
-    inherit version;
+    inherit openlilylib version;
 
     buildInputs = [ lilypond-with-improviso ];
   };
 
-  scoresToEngrave =
+  engravedScores =
     if (isNull scores || !isString scores)
        then songs.all
        else map (songName: mkSong { inherit songName; })
                 (splitString " " scores);
 
-  drv = stdenv.mkDerivation rec {
+  drv = stdenv.mkDerivation {
     name = "naptime-${version}";
     inherit version;
-    src = ./.;
+    src = ./songs;
 
-    patchPhase = deletePdfs;
-
-    buildInputs = [ lilypond-with-improviso ] ++ scoresToEngrave;
+    nativeBuildInputs = engravedScores;
 
     buildPhase = "true";
 
     installPhase = ''
       install -m755 -d $out
       ${concatMapStringsSep "\n"
-          (song: "install -m644 ${song}/*.pdf $out")
-          scoresToEngrave}
+          (score: "install -m644 -t $out ${score}/*")
+          engravedScores}
     '';
     # TODO: meta
   };
 in
 
-if inNixShell
-   then env
-   else drv
+# if inNixShell then env else drv
+{
+  env = env;
+  drv = drv;
+  songs = songs;
+}
