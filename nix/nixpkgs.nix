d5dd6ffc4b96cf5d56e921b6dafd2f11fdf80bb8
Eric Bailey <eric@ericb.me>
2017-05-26 05:13:14 -0500

    nixpkgs: 3ea16e9 -> e284740

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -3,6 +3,6 @@ with import <nixpkgs> { };
 fetchFromGitHub {
   owner = "NixOS";
   repo = "nixpkgs";
-  rev = "3ea16e9";
-  sha256 = "1043zkz69arqax62anb0wqq7bl7gj7axm8h7dcsmy2ph1agr242a";
+  rev = "e284740dd5265a8f30699692aa1bb59df5dfb9d6";
+  sha256 = "1qna2bz1950cn54ih2glpgpjv6jvf3rdjjyac9jl1gbi9433fmib";
 }
