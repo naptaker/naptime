8fcd5ab49cce0f1250b2d830b92c8c3191ae2821
Eric Bailey <eric@ericb.me>
2020-12-17 07:08:44 -0600

    shell.nix: Add frescobaldi

--- shell.nix
+++ shell.nix
@@ -1,9 +1,12 @@
 { pkgs ? import ./nix }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    (frescobaldi.override {
+      lilypond = lilypond-with-improviso;
+    })
     lilypond-with-improviso
     # pythonPackages.pywatchman
-    # timidity
+    timidity
   ];
   LILYPOND_SHARE_DIR = "${pkgs.lilypond-unstable}/share";
 }
